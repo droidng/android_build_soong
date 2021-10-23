c9a4ad65fdec6cd1ce6511f4a0350a7dbeec2f67
spezi77 <spezi7713@gmx.net>
2021-10-23 14:48:16 +0200

    soong: Allow aarch64-linux-android-{ar,as,ld,objcopy,objdump,nm}
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: Ie81b348c28459f94d6c77d64b524e7a8e12da890

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -98,10 +98,18 @@ var Configuration = map[string]PathConfig{
 	"tr":      Allowed,
 	"unzip":   Allowed,
 	"zip":     Allowed,
-	"arm-linux-androidkernel-as": Allowed,
-	"arm-linux-androidkernel-ld": Allowed,
 
+	// Kernel tools
+	"dtc":     Allowed,
 	"mkdtimg": Allowed,
+	"arm-linux-androidkernel-as":    Allowed,
+	"arm-linux-androidkernel-ld":    Allowed,
+	"aarch64-linux-android-ar":      Allowed,
+	"aarch64-linux-android-as":      Allowed,
+	"aarch64-linux-android-ld":      Allowed,
+	"aarch64-linux-android-objcopy": Allowed,
+	"aarch64-linux-android-objdump": Allowed,
+	"aarch64-linux-android-nm":      Allowed,
 
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
